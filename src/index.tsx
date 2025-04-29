833800007c56e6fe8d8ce6413495c76c8f61e97a
Hilary Luo <hluo@clearpathrobotics.com>
2025-04-29 10:18:41 -0400

    Update copyrights

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 /*
- * This file is part of Cockpit.
+ * This file is part of Cockpit ROS 2 Diagnostics.
  *
- * Copyright (C) 2017 Red Hat, Inc.
+ * Copyright (C) 2025 Clearpath Robotics, Inc., a Rockwell Automation Company.
  *
- * Cockpit is free software; you can redistribute it and/or modify it
+ * Cockpit ROS 2 Diagnostics is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
- * Cockpit is distributed in the hope that it will be useful, but
+ * Cockpit ROS 2 Diagnostics is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
