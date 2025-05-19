5b862cdc952ee7497ca682d6e093cb2b8fe758fc
Hilary Luo <hluo@clearpathrobotics.com>
2025-05-18 22:49:58 -0400

    Add copyrights and attribution in files

--- src/roslib/index.ts
+++ src/roslib/index.ts
@@ -1,2 +1,24 @@
+/*
+ * This file is part of Cockpit ROS 2 Diagnostics but was originally sourced from
+ * https://github.com/tier4/roslibjs-foxglove under the Apache License 2.0.
+ *
+ * Modifications made by Hilary Luo 2025.
+ *
+ * Copyright (C) 2025 Clearpath Robotics, Inc., a Rockwell Automation Company.
+ *
+ * Cockpit ROS 2 Diagnostics is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * Cockpit ROS 2 Diagnostics is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Cockpit; If not, see <http://www.gnu.org/licenses/>.
+ */
+
 export { Ros } from './Ros';
 export { Topic, Message } from './Topic';
