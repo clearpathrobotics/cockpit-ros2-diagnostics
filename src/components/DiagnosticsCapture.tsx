beb380445b324e03af2a7f99f8309fce931b00f4
Hilary Luo <103377417+hilary-luo@users.noreply.github.com>
2025-07-08 14:40:28 -0400

    Fix/diag cap (#33)
    * Make sure all clearpath related commands are in the same section
    
    * Try and catch for each diag capture command
    
    * Ensure that daemon has ROS_SUPER_CLIENT true in case Discovery Server is being used
    
    * Alert user if diagnostic capture is incomplete

--- src/components/DiagnosticsCapture.tsx
+++ src/components/DiagnosticsCapture.tsx
@@ -32,6 +32,9 @@ export const DiagnosticsCapture = ({ namespace }: { namespace: string }) => {
         setErrorMessage(null);
         setDownloadPath(null);
 
+        let failedCommand = false;
+        let failedCommandMessage = "Incomplete diagnostic capture: Failed to execute commands: ";
+
         try {
             const temp_folder = (await runBash("mktemp -d")).trim();
 
@@ -44,11 +47,12 @@ export const DiagnosticsCapture = ({ namespace }: { namespace: string }) => {
             const commands_usr = [
                 `lsusb -t -v >> ${temp_folder}/usb.log`,
                 `ls -lisah /dev/ >> ${temp_folder}/dev.log`,
-                `source /etc/clearpath/setup.bash && env | grep -e ROS -e RMW >> ${temp_folder}/env.log`,
                 `ip a >> ${temp_folder}/ip.log`,
             ];
 
             const commands_clearpath = [
+                `[ -f "/etc/clearpath/setup.bash" ] && source /etc/clearpath/setup.bash && ros2 daemon stop`,
+                `[ -f "/etc/clearpath/setup.bash" ] && source /etc/clearpath/setup.bash && export ROS_SUPER_CLIENT=True && ros2 daemon start`,
                 `mkdir -p ${temp_folder}/services`,
                 `journalctl -b 0 -u clearpath-platform >> ${temp_folder}/services/platform.log`,
                 `journalctl -b 0 -u clearpath-platform-extras >> ${temp_folder}/services/platform_extras.log`,
@@ -59,36 +63,63 @@ export const DiagnosticsCapture = ({ namespace }: { namespace: string }) => {
                 `journalctl -b 0 -u clearpath-discovery >> ${temp_folder}/services/discovery_server.log`,
                 `journalctl -b 0 -u clearpath-zenoh-router >> ${temp_folder}/services/zenoh_router.log`,
                 `[ -f "/etc/clearpath/robot.yaml" ] && cp /etc/clearpath/robot.yaml ${temp_folder}/robot.yaml`,
+                `[ -f "/etc/clearpath/setup.bash" ] && source /etc/clearpath/setup.bash && env | grep -e ROS -e RMW >> ${temp_folder}/env.log`,
                 `[ -f "/etc/clearpath/setup.bash" ] && source /etc/clearpath/setup.bash && ros2 doctor --report >> ${temp_folder}/ros2_doctor.log`,
                 `[ -f "/etc/clearpath/setup.bash" ] && source /etc/clearpath/setup.bash && ros2 topic echo --timeout 10 ${namespace}/diagnostics_agg >> ${temp_folder}/ros2_diagnostics.log`, // Timeout to avoid hanging if no messages are published
             ];
 
             for (const command of commands_su) {
-                const output = await runBash(command, { superuser: "require" });
-                console.log("Command executed successfully:", command, ": ", output);
+                try {
+                    const output = await runBash(command, { superuser: "require" });
+                    console.log("Command executed successfully:", command, ": ", output);
+                } catch (error) {
+                    console.error("Error executing command:", command, error);
+                    failedCommandMessage += command + "; ";
+                    failedCommand = true;
+                    continue; // Skip to the next command if one fails
+                }
             }
 
             for (const command of commands_usr) {
-                const output = await runBash(command);
-                console.log("Command executed successfully:", command, ": ", output);
+                try {
+                    const output = await runBash(command);
+                    console.log("Command executed successfully:", command, ": ", output);
+                } catch (error) {
+                    console.error("Error executing command:", command, error);
+                    failedCommandMessage += command + "; ";
+                    failedCommand = true;
+                    continue; // Skip to the next command if one fails
+                }
             }
 
             if (await runBash("test -f /etc/clearpath/robot.yaml && echo 'exists'")) {
                 for (const command of commands_clearpath) {
-                    const output = await runBash(command);
-                    console.log("Command executed successfully:", command, ": ", output);
+                    try {
+                        const output = await runBash(command);
+                        console.log("Command executed successfully:", command, ": ", output);
+                    } catch (error) {
+                        console.error("Error executing command:", command, error);
+                        failedCommandMessage += command + "; ";
+                        failedCommand = true;
+                        continue; // Skip to the next command if one fails
+                    }
                 }
             }
 
             const hostname = (await runBash("hostname")).trim();
             const home = (await runBash("echo $HOME")).trim();
             const current_datetime = (await runBash("date +%Y-%m-%d_%H-%M-%S")).trim();
-            const archive_name = `${home}/diagnostic_captures/${hostname}_${current_datetime}.tar.gz`;
+            let archive_name = `${home}/diagnostic_captures/${hostname}_${current_datetime}`;
+            if (failedCommand) archive_name += "_incomplete";
+            archive_name += ".tar.gz";
             console.log("Archive name:", archive_name);
             await runBash(`mkdir -p ${home}/diagnostic_captures`);
             await runBash(`cd ${temp_folder} && tar -czvf ${archive_name} .`, { superuser: "require" });
             console.log("tar command executed successfully");
             await runBash(`rm -rf ${temp_folder}`, { superuser: "require" });
+            if (failedCommand) {
+                setErrorMessage(failedCommandMessage + "View console log for details.");
+            }
 
             setDownloadPath(archive_name);
         } catch (error) {
